a96432d280952646a7e1cc7ba206589265547aed
Ana Valdivia <anav1317@yahoo.com>
2020-03-31 16:33:15 -0700

    Fixed issue

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -19,8 +19,8 @@ router.get("/", function(req, res) {
   router.post("/api/burgers", function(req, res) {
     console.log(req.body);
     burger.insertOne(["burger_name"], [req.body.burger], function(result) {
-
       // res.json({ id: result.insertId });
+      res.redirect("/");
     });
   });
   
